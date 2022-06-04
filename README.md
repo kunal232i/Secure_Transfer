8060c61ad26784003d20de3a4f218f3c5b7804f4
kunal232i <kingawale23@gmail.com>
2022-06-04 09:17:29 +0000

    initial commit

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Crowd Funding Platform in Web3.0
 ./crowdfunding
 npm install
 npm run dev
+
+// .jsx for React applications
--- crowdfunding/src/App.jsx
+++ crowdfunding/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
+import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
 import './App.css'
-
-const App = () => {
-  return (
-    <div className="App">
-     <h1 class="text-3xl font-bold underline">
-      Hello world!
-     </h1>
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Welcome />
     </div>
-  )
-}
+    <Services />
+    <Transactions />
+    <Footer />
+  </div>
+);
 
-export default App
+export default App;
