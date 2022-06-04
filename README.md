77644d7deb99570856378957a016a18f29987b99
kunal232i <kingawale23@gmail.com>
2022-06-04 10:50:37 +0000

    Welcome component

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ npm install
 npm install react-icons ethers
 npm run dev
 
+47.54 V
 // .jsx for React applications
--- crowdfunding/vite.config.js
+++ crowdfunding/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  server: {
+    hmr: {
+      protocol: "ws",
+      port: 3001,
+      host: "0.0.0.0",
+    },
+  },
+});
